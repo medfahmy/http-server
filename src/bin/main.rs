f70c0f49b757b9fa9d750f106c269a0ff8ef4796
medfahmy <medfahmy99@gmail.com>
2022-03-22 18:14:16 +0100

    thread pool for requests

---
+++ src/bin/main.rs
@@ -0,0 +1,52 @@
+use web_server::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::time::Duration;
+use std::io::prelude::*;
+use std::fs;
+use std::thread;
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let get = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
+
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else if buffer.starts_with(sleep) {
+        thread::sleep(Duration::from_secs(10));
+        ("HTTP/1.1 200 OK", "hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents,
+    );
+
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
+}
+
+fn run_server(port: usize) {
+    let listener = TcpListener::bind(format!("127.0.0.1:{}", port)).unwrap();
+    let pool = ThreadPool::new(4);
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        // thread::spawn(|| handle_connection(stream));
+        pool.execute(|| handle_connection(stream));
+    }
+}
+
+fn main() {
+    run_server(7878);
+}
