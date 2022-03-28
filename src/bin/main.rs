ec5514e1f6b1b68d54493907d337060fb59651e8
medfahmy <medfahmy99@gmail.com>
2022-03-28 12:31:44 +0000

    static

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,12 +15,12 @@ fn handle_connection(mut stream: TcpStream) {
     let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = if buffer.starts_with(get) {
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "static/hello.html")
     } else if buffer.starts_with(sleep) {
         thread::sleep(Duration::from_secs(10));
-        ("HTTP/1.1 200 OK", "hello.html")
+        ("HTTP/1.1 200 OK", "static/hello.html")
     } else {
-        ("HTTP/1.1 404 NOT FOUND", "404.html")
+        ("HTTP/1.1 404 NOT FOUND", "static/404.html")
     };
 
     let contents = fs::read_to_string(filename).unwrap();
